fcc8f6352aafb79080fd0cca3d33e8c4325ad5c3
mvjl000 <milosz.devmail@gmail.com>
2021-05-06 16:43:01 +0200

    Prepare project structure

---
+++ pages/_document.tsx
@@ -0,0 +1,46 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  // @ts-ignore
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // @ts-ignore
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Wrapper } from '../components/HomePage/HomePage.styles';
+
+const HomePage: React.FC = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default HomePage;
