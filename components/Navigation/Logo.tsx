f9468aa0a844d246d55fd113c8d806b893b5e47d
mvjl000 <milosz.devmail@gmail.com>
2021-05-06 21:09:27 +0200

    Create Logo component

---
+++ components/Navigation/Logo.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  p {
+    font-family: 'Quicksand', sans-serif;
+    color: white;
+    font-weight: 600;
+    font-size: 18px;
+    @media (min-width: 1024px) {
+      font-size: 20px;
+    }
+  }
+`;
+
+const Logo: React.FC = () => (
+  <Wrapper>
+    <p>Miłosz</p>
+    <p>Piskadło</p>
+  </Wrapper>
+);
+
+export default Logo;
