293eca8f2302eebc34d9a64a30dcf94eaee4cf99
mvjl000 <milosz.devmail@gmail.com>
2021-05-06 21:10:02 +0200

    Create Navigation component

---
+++ components/Navigation/Navigation.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 25px;
+`;
---
+++ components/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import Logo from './Logo';
+import { Wrapper } from './Navigation.styles';
+
+const Navigation: React.FC = () => {
+  return (
+    <Wrapper>
+      <Logo />
+    </Wrapper>
+  );
+};
+
+export default Navigation;
