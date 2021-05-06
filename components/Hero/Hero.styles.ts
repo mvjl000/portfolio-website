ad4e6d6e20e053c17aa0e8c345710f5887dbdb01
mvjl000 <milosz.devmail@gmail.com>
2021-05-06 21:40:39 +0200

    Scroll down button

--- components/Hero/Hero.styles.ts
+++ components/Hero/Hero.styles.ts
@@ -15,3 +15,33 @@ export const ProfessionHeading = styled.h1`
   font-style: italic;
   text-align: center;
 `;
+
+export const ArrowWrapper = styled.div`
+  position: absolute;
+  bottom: 10%;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 350px;
+  p {
+    color: white;
+    font-size: 20px;
+    font-weight: 300;
+    margin-bottom: 10px;
+    @media (min-width: 1024px) {
+      font-size: 26px;
+      margin-bottom: 20px;
+    }
+  }
+  svg {
+    color: white;
+    transform: scale(1.5);
+    cursor: pointer;
+    @media (min-width: 1024px) {
+      transform: scale(2.2);
+    }
+  }
+`;
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,9 +1,14 @@
-import { Wrapper, ProfessionHeading } from './Hero.styles';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import { Wrapper, ProfessionHeading, ArrowWrapper } from './Hero.styles';
 
 const Hero: React.FC = () => {
   return (
     <Wrapper>
       <ProfessionHeading>Frontend Developer</ProfessionHeading>
+      <ArrowWrapper>
+        <p>Get to know me better</p>
+        <KeyboardArrowDownIcon />
+      </ArrowWrapper>
     </Wrapper>
   );
 };
