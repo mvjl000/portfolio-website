8578c0964c2daef631580c3544a89fac762a4c12
mvjl000 <milosz.devmail@gmail.com>
2021-05-06 21:31:24 +0200

    Add icons to navigation

--- components/Navigation/Navigation.styles.ts
+++ components/Navigation/Navigation.styles.ts
@@ -6,7 +6,29 @@ export const Wrapper = styled.nav`
   left: 0;
   width: 100%;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: space-between;
   padding: 15px 25px;
+`;
+
+export const IconsWrapper = styled.aside`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: auto;
+  height: 150px;
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    height: auto;
+    width: 200px;
+  }
+  svg {
+    color: white;
+    transform: scale(1.4);
+    cursor: pointer;
+    @media (min-width: 1024px) {
+      transform: scale(1.5) translateY(10px);
+    }
+  }
 `;
--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,10 +1,31 @@
+import Link from 'next/link';
 import Logo from './Logo';
-import { Wrapper } from './Navigation.styles';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { Wrapper, IconsWrapper } from './Navigation.styles';
 
 const Navigation: React.FC = () => {
   return (
     <Wrapper>
       <Logo />
+      <IconsWrapper>
+        <Link href="https://www.instagram.com/mvjl000/">
+          <a target="_blank">
+            <InstagramIcon />
+          </a>
+        </Link>
+        <Link href="https://github.com/mvjl000">
+          <a target="_blank">
+            <GitHubIcon />
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/mi%C5%82osz-piskad%C5%82o-477a2a1b5/">
+          <a target="_blank">
+            <LinkedInIcon />
+          </a>
+        </Link>
+      </IconsWrapper>
     </Wrapper>
   );
 };
