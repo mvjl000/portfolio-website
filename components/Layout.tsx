e66eb7dcc06ac0d81450127df18c8c50becde8fd
mvjl000 <milosz.devmail@gmail.com>
2021-05-07 13:22:24 +0200

    Implement Menu into layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
+import Menu from './Navigation/Menu/Menu';
 import Navigation from './Navigation/Navigation';
 
 const Layout: React.FC = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    isMenuOpen
+      ? document.body.classList.add('body-lock')
+      : document.body.classList.remove('body-lock');
+  }, [isMenuOpen]);
+
   return (
     <>
-      <Navigation />
+      <Navigation isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <main>{children}</main>
     </>
   );
