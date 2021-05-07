3cf6f93aebd01e05840d783f64eeffe0463b6ba6
mvjl000 <milosz.devmail@gmail.com>
2021-05-07 13:21:40 +0200

    Change Navigation logic

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,14 +1,27 @@
-import { useState } from 'react';
-import Link from 'next/link';
+import { useEffect, Dispatch, SetStateAction } from 'react';
+import { useRouter } from 'next/router';
+// import Link from 'next/link';
 import Logo from './Logo';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+// import InstagramIcon from '@material-ui/icons/Instagram';
+// import GitHubIcon from '@material-ui/icons/GitHub';
+// import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { Wrapper, IconsWrapper } from './Navigation.styles';
 import Burger from './Burger/Burger';
 
-const Navigation: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+interface NavigationProps {
+  isMenuOpen: boolean;
+  setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const Navigation: React.FC<NavigationProps> = ({
+  isMenuOpen,
+  setIsMenuOpen,
+}) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [router]);
 
   return (
     <Wrapper>
@@ -32,7 +45,7 @@ const Navigation: React.FC = () => {
       </IconsWrapper> */}
       <Burger
         onButtonClick={() => setIsMenuOpen(!isMenuOpen)}
-        isActive={isMenuOpen}
+        isOpenVariant={true}
       />
     </Wrapper>
   );
