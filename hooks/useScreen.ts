aceb09b3692ac6baf09f8111eb7de383966c51db
mvjl000 <milosz.devmail@gmail.com>
2021-05-08 17:37:13 +0200

    Fix useScreen hook logic

--- hooks/useScreen.ts
+++ hooks/useScreen.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 export const useScreen = () => {
   const [isDesktopMode, setIsDesktopMode] = useState(
-    typeof window !== 'undefined' ? window.innerWidth > 1023 : false
+    typeof window !== 'undefined' && window.innerWidth > 1023
   );
 
   const toggleMode = () => {
