4d1cce6f369a7f0fd1a99f89c9d54157dddff52e
mvjl000 <milosz.devmail@gmail.com>
2021-05-09 20:48:28 +0200

    Request to headless cms for projects

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {
+    CMS_URL: process.env.CMS_URL,
+    CMS_TOKEN: process.env.CMS_TOKEN,
+  },
+};
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,60 @@
 import { NextPage } from 'next';
+import axios from 'axios';
 import {
   Wrapper,
   Heading,
   Subheading,
 } from '../components/Projects/Project.styles';
 import Project from '../components/Projects/Project/Project';
+import { ProjectTypes } from '../types/project';
 
-const ProjectsPage: NextPage = () => {
+interface ProjectsProps {
+  projects: ProjectTypes[];
+}
+
+export const getStaticProps = async () => {
+  const query = `
+         {
+          allProjects {
+            id,
+            title,
+            description,
+            date,
+            techStack,
+            image {
+              url,
+              alt
+            }
+        	}
+        }
+      `;
+
+  let projects: ProjectTypes[] = [];
+
+  try {
+    const responseData = await axios.post(
+      `${process.env.CMS_URL}`,
+      {
+        query,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${process.env.CMS_TOKEN}`,
+        },
+      }
+    );
+    projects = responseData.data.data.allProjects;
+  } catch (error) {
+    console.log(error);
+  }
+  return {
+    props: {
+      projects,
+    },
+  };
+};
+
+const ProjectsPage: NextPage<ProjectsProps> = ({ projects }) => {
   return (
     <Wrapper>
       <Heading>Projects & other activity</Heading>
@@ -14,7 +62,9 @@ const ProjectsPage: NextPage = () => {
         Here I upload all of my side projects and contributions, take a look at
         them 😀
       </Subheading>
-      <Project />
+      {projects.map((prj) => (
+        <Project project={prj} />
+      ))}
     </Wrapper>
   );
 };
