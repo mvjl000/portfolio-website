93a9ea0cc7e1068e40a487bae51765582e0fa851
mvjl000 <milosz.devmail@gmail.com>
2021-05-10 10:56:12 +0200

    Create tech page

---
+++ pages/tech.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const TechPage: NextPage = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <h1>World</h1>
+    </>
+  );
+};
+
+export default TechPage;
