f9ef83fd961a00681002935f54376507b423b770
mvjl000 <milosz.devmail@gmail.com>
2021-05-10 11:46:32 +0200

    Create tech page heading

---
+++ components/Tech/Tech.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  padding-top: 120px;
+  background-color: #fff;
+`;
+
+export const Heading = styled.h1`
+  margin: 20px 50px;
+  color: #111;
+  font-size: 66px;
+  font-weight: 600;
+  font-family: 'Oswald', sans-serif;
+  text-align: center;
+  @media (min-width: 1024px) {
+    margin-top: 50px;
+    margin-left: 70px;
+    font-size: 74px;
+  }
+`;
--- pages/tech.tsx
+++ pages/tech.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from 'next';
+import { Wrapper, Heading } from '../components/Tech/Tech.styles';
 
 const TechPage: NextPage = () => {
   return (
-    <>
-      <h1>Hello</h1>
-      <h1>World</h1>
-    </>
+    <Wrapper>
+      <Heading>Technologies I use on a daily basis</Heading>
+    </Wrapper>
   );
 };
 
