a1fbe323a301edcce8f24f87e252655a43cbcb35
mvjl000 <milosz.devmail@gmail.com>
2021-05-11 22:17:59 +0200

    Api route for nodemailer

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ module.exports = {
   env: {
     CMS_URL: process.env.CMS_URL,
     CMS_TOKEN: process.env.CMS_TOKEN,
+    USER_MAIL: process.env.USER_MAIL,
+    USER_PASSWORD: process.env.USER_PASSWORD,
+    DESTINATION_MAIL: process.env.DESTINATION_MAIL,
   },
   images: {
     domains: ['www.datocms-assets.com'],
--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,7 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body);
+  const { email, message } = req.body;
+
+  let nodemailer = require('nodemailer');
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    auth: {
+      user: process.env.USER_MAIL,
+      pass: process.env.USER_PASSWORD,
+    },
+    secure: true,
+  });
+  const mailData = {
+    from: process.env.USER_MAIL,
+    to: process.env.DESTINATION_MAIL,
+    subject: `Message From ${email}`,
+    text: message,
+    html: `<div><h3>${email}</h3><p>${message}</p></div>`,
+  };
+  //@ts-ignore
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+
+  res.status(200);
 };
 
 export default handler;
