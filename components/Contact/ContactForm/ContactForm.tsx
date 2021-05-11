a0b4fb4457b370ae39373107c6d76d23a982fefb
mvjl000 <milosz.devmail@gmail.com>
2021-05-11 21:28:13 +0200

    Handling form change and contact form submit

--- components/Contact/ContactForm/ContactForm.tsx
+++ components/Contact/ContactForm/ContactForm.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useTranslation } from '../../../hooks/useTranslation';
 import {
   StyledForm,
@@ -7,17 +8,47 @@ import {
 } from './ContactForm.styles';
 
 const ContactForm: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   const { translate } = useTranslation();
 
+  const handleFieldChange = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    if (event.target.name === 'email') {
+      setEmail(event.target.value);
+    } else {
+      setMessage(event.target.value);
+    }
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(email, message);
+  };
+
   return (
-    <StyledForm>
+    <StyledForm onSubmit={handleFormSubmit}>
       <StyledInput>
         <label htmlFor="email">Email:</label>
-        <input id="email" type="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleFieldChange}
+        />
       </StyledInput>
       <StyledTextArea>
         <label htmlFor="message">{translate('contactFormMessage')}:</label>
-        <textarea id="message"></textarea>
+        <textarea
+          id="message"
+          name="message"
+          value={message}
+          onChange={handleFieldChange}
+        ></textarea>
       </StyledTextArea>
       <Button type="submit">
         {translate('contactFormButton')} <img src="/images/plane.png" />
